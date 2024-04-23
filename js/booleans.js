32e33cdd18b2b3e0b176941bc17b90462b167fac
enriqueomarfarias <enrikefarias@gmail.com>
2024-04-23 10:48:01 -0300

    booleans

---
+++ js/booleans.js
@@ -0,0 +1,15 @@
+let verdadero = true;
+let falso = false;
+
+let v = Boolean(true);
+let f = Boolean(false);
+
+console.log(verdadero, falso);
+console.log(typeof verdadero, typeof falso);
+
+// Valores que tienden a verdadero o falso
+console.log(Boolean(0));
+console.log(Boolean(-1));
+console.log(Boolean(""));
+console.log(Boolean(" "));
+console.log(Boolean(3));
