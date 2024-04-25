711ff12328e6e5dd59ba1e3652f95aff443d06cb
enriqueomarfarias <enrikefarias@gmail.com>
2024-04-25 11:33:43 -0300

    undefined

---
+++ js/undefined-null-nan.js
@@ -0,0 +1,12 @@
+//undefined indica que no se ha inicializado una variable y que el valor esta ausente
+let indefinida;
+console.log(indefinida);
+
+// null es un valor especial que indica la ausencia de un valor
+let nulo = null;
+console.log(nulo);
+
+//NaM - Not a Numbrer
+
+let noEsUnNumero = "hola" * 3.2;
+console.log(noEsUnNumero);
